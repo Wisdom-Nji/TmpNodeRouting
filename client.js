6751820a48d3fc1018e0eadba6529e922b00bddc
sherlock <wisdomnji@gmail.com>
2019-06-20 19:58:13 +0100

    changes

--- client.js
+++ client.js
@@ -9,7 +9,7 @@ function establishServerConnection() {
 	//serverConnection.setKeepAlive(true, 2000);
 
 	serverConnection.on('connect', function() {
-		console.log("Successfully connected to host ** my prayer is I won't shit the bed!! **");
+		console.log("Connected to Afkanerd OpenOs | Main Server | Cloud Instance");
 		serverConnection.sendMessage(JSON.stringify("I'm connected: Now send in those sms things"));
 		serverConnection.setKeepAlive(true, 2000);
 	})
@@ -25,28 +25,34 @@ function establishServerConnection() {
 		console.info(`Received command for ${data.length} message(s)`);
 		//serverConnection.sendMessage(`Received command for ${data.length} message(s)`);
 		if(typeof data[data.length -1] != "undefined" && Object.keys(data[data.length -1]) == "messageId") {
-			console.log(`Message ID: ${data[data.length -1].messageId}`);
-			serverConnection.sendMessage(JSON.stringify({
-				type : "confirmation",
-				messageId : data[data.length -1].messageId
-			}));
-			delete data[data.length -1];
-			for(var i in data) {
-				console.info("Processing an SMS message");
-				console.info(data[i]);
-				var number = data[i].number;
-				var group = data[i].service_provider.toUpperCase();
-				var message = data[i].message + "\n\nPowered by Afkanerd OpenOs";
-				var _class = "1";
-				const system_command = spawnSync('afsms', [
-					'--send',
-					'--number',number,
-					'--message',message,
-					'--group',group,
-					'--class',_class
-				]);
-				//console.log("Terminal output\n" + system_command.output);
-				//console.log("Terminal return code: " + system_command.status);
+			try {
+				console.log(`Message ID: ${data[data.length -1].messageId}`);
+				serverConnection.sendMessage(JSON.stringify({
+					type : "confirmation",
+					messageId : data[data.length -1].messageId
+				}));
+				delete data[data.length -1];
+				for(var i in data) {
+					console.info("Processing an SMS message");
+					console.info(data[i]);
+					var number = data[i].number;
+					var group = data[i].service_provider.toUpperCase();
+					var message = data[i].message + "\n\nPowered by Afkanerd OpenOs";
+					var _class = "1";
+					const system_command = spawnSync('afsms', [
+						'--send',
+						'--number',number,
+						'--message',message,
+						'--group',group,
+						'--class',_class
+					]);
+					//console.log("Terminal output\n" + system_command.output);
+					//console.log("Terminal return code: " + system_command.status);
+				}
+			}
+			catch(error) {
+				console.log("Some error occured, sending it back to server!");
+				serverConnection.sendMessage(error);
 			}
 
 		}
